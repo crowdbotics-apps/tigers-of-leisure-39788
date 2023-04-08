218ba1abd8b40651f650d06f0db7d8183eca0e9a
Crowdbotics <team@crowdbotics.com>
2023-04-08 03:01:32 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tigers-of-leisure-39788",
+  url: "https://tigers-of-leisure-39788.botics.co",
+  api: "https://tigers-of-leisure-39788.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
